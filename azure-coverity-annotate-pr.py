cb0530fa2e5bf1cba72bbd3268f074309fba9f08
James Croall <jcroall@pobox.com>
2022-01-30 13:58:16 +0000

    Update

--- azure-coverity-annotate-pr.py
+++ azure-coverity-annotate-pr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import json
+import re
 import sys
 import os
 import argparse
@@ -11,7 +12,7 @@ import base64
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-        description='Post Coverity issue summary to Azure Repos Pull Request Threads')
+                                 description='Post Coverity issue summary to Azure Repos Pull Request Threads')
 parser.add_argument('--debug', default=0, help='set debug level [0-9]')
 parser.add_argument('--coverity-json', default='coverity-results.json', help='Coverity output JSON')
 
@@ -23,10 +24,76 @@ jsonFile = args.coverity_json
 
 # Process output from Polaris CLI
 with open(jsonFile) as f:
-  data = json.load(f)
+    data = json.load(f)
 
 print("INFO: Reading Coverity incremental analysis results from " + jsonFile)
-if(debug): print("DEBUG: " + json.dumps(data, indent = 4, sort_keys=True) + "\n")
+if (debug): print("DEBUG: " + json.dumps(data, indent=4, sort_keys=True) + "\n")
+
+# Get a list of all merge keys seen in analysis
+seen_in_analysis = dict()
+for issue in data["issues"]:
+    seen_in_analysis[issue['mergeKey']] = 1
+
+seen_in_comments = dict()
+
+# Get list of open threads
+# GET https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/pullRequests/{pullRequestId}/threads?api-version=6.0
+
+SYSTEM_COLLECTIONURI = os.getenv('SYSTEM_COLLECTIONURI')
+SYSTEM_PULLREQUEST_PULLREQUESTID = os.getenv('SYSTEM_PULLREQUEST_PULLREQUESTID')
+SYSTEM_TEAMPROJECT = os.getenv('SYSTEM_TEAMPROJECT')
+BUILD_REPOSITORY_ID = os.getenv('BUILD_REPOSITORY_ID')
+url = f"{SYSTEM_COLLECTIONURI}{SYSTEM_TEAMPROJECT}/_apis/git/repositories/{BUILD_REPOSITORY_ID}/pullRequests/{SYSTEM_PULLREQUEST_PULLREQUESTID}/threads?api-version=6.0"
+
+accessToken = os.getenv('SYSTEM_ACCESSTOKEN')
+authorization = str(base64.b64encode(bytes(':' + accessToken, 'ascii')), 'ascii')
+
+headers = {
+    'Accept': 'application/json',
+    'Authorization': 'Basic ' + authorization
+}
+
+if (debug): print("DEBUG: perform API Call to ADO" + url + "\n")
+r = requests.get(url=url, headers=headers)
+if r.status_code == 200:
+    if (debug): print("DEBUG: Success")
+else:
+    print(f"ERROR: Unable to get PR threads from Azure DevOps. Error code: {r.status_code}")
+    print(r.text)
+    sys.exit(1)
+
+for thread in r.json()['value']:
+    if debug: print(f"DEBUG: Thread={json.dumps(thread, indent=4, sort_keys=True)}")
+
+    for comment in thread['comments']:
+        match = re.search('<!-- Coverity (\S+) -->', comment['content'])
+        if match:
+            coverity_mk = match.group(1)
+            seen_in_comments[coverity_mk] = 1
+            if debug: print(f"DEBUG: Found Coverity comment for {coverity_mk}")
+            if coverity_mk not in seen_in_analysis:
+                print(f"DEBUG:  Not seen in analysis")
+
+                url = f"{SYSTEM_COLLECTIONURI}{SYSTEM_TEAMPROJECT}/_apis/git/repositories/{BUILD_REPOSITORY_ID}/pullRequests/{SYSTEM_PULLREQUEST_PULLREQUESTID}/threads/{thread['id']}/comments/{comment['id']}?api-version=6.0"
+
+                accessToken = os.getenv('SYSTEM_ACCESSTOKEN')
+                authorization = str(base64.b64encode(bytes(':' + accessToken, 'ascii')), 'ascii')
+
+                headers = {
+                  'Accept': 'application/json',
+                  'Authorization': 'Basic ' + authorization
+                }
+
+                if (debug): print("DEBUG: perform API Call to ADO" + url + "\n")
+                r = requests.delete(url=url, headers=headers)
+                if r.status_code == 200:
+                  if (debug): print("DEBUG: Success")
+                else:
+                  print(f"ERROR: Unable to Delete thread id={thread['id']} comment={comment['id']} from Azure DevOps. Error code: {r.status_code}")
+                  print(r.text)
+                  sys.exit(1)
+
+####################
 
 # Loop through found issues for specified merge keys, and build out output map
 # TODO: Can there be multiple entries for the merge key? I think the right thing would be to list all of them.
@@ -37,61 +104,84 @@ vulnerabilities = []
 azComments = []
 
 for item in data["issues"]:
-  checkerName = item["checkerName"]
-  checkerProperties = item["checkerProperties"]
-  subcategoryShortDescription = checkerProperties["subcategoryShortDescription"]
-  subcategoryLongDescription = checkerProperties["subcategoryLongDescription"]
-  cwe = checkerProperties["cweCategory"]
-  impact = checkerProperties["impact"]
-  codeLangauge = item["code-language"]
-  mergeKey = item["mergeKey"]
-  strippedMainEventFilePathname = item["strippedMainEventFilePathname"]
-  mainEventLineNumber = item["mainEventLineNumber"]
 
-  eventNumber = 1
-  description = ""
-  start_line = 0
-  location = dict();
+    if issue['mergeKey'] in seen_in_comments:
+        if debug: print(f"DEBUG: Merge key {issue['mergeKey']} already seen in comments, do not create another comment")
+        continue
 
-  for event in item["events"]:
-    if event["main"]:
-      location["file"] = event["strippedFilePathname"]
-      start_line = event["lineNumber"]
-      description = description + "" + event["eventDescription"]
+    checkerName = item["checkerName"]
+    checkerProperties = item["checkerProperties"]
+    subcategoryShortDescription = checkerProperties["subcategoryShortDescription"]
+    subcategoryLongDescription = checkerProperties["subcategoryLongDescription"]
+    cwe = checkerProperties["cweCategory"]
+    impact = checkerProperties["impact"]
+    codeLangauge = item["code-language"]
+    mergeKey = item["mergeKey"]
+    strippedMainEventFilePathname = item["strippedMainEventFilePathname"]
+    mainEventLineNumber = item["mainEventLineNumber"]
 
-    if event["remediation"]:
-      description = description + "\n\n" + event["eventDescription"]
 
-  newComment = dict()
+    eventNumber = 1
+    description = None
+    remediation = None
+    start_line = 0
+    location = dict()
 
-  comments = []
-  comment = dict()
-  comment["parentCommentId"] = 0
-  comment["commentType"] = 1
-  commentContent = ":warning: Coverity Static Analysis found this issue with your code:\n\n" + description + "\n\n[View the full issue report in Coverity](http://synopsys.com)"
-  comment["content"] = commentContent
-  comments.append(comment)
-  newComment["comments"] = comments
+    for event in item["events"]:
+        if event["main"]:
+            location["file"] = event["strippedFilePathname"]
+            start_line = event["lineNumber"]
+            description = event["eventDescription"]
 
-  threadContext = dict()
+        if event["remediation"]:
+            remediation = event["eventDescription"]
 
-  rightFileEnd = dict()
-  rightFileEnd["line"] = start_line
-  rightFileEnd["offset"] = 1
+    newComment = dict()
 
-  rightFileStart = dict()
-  rightFileStart["line"] = start_line
-  rightFileStart["offset"] = 1
+    comments = []
+    comment = dict()
+    comment["parentCommentId"] = 0
+    comment["commentType"] = 1
 
-  threadContext["filePath"] = "/" + location["file"]
-  threadContext["rightFileEnd"] = rightFileEnd
-  threadContext["rightFileStart"] = rightFileStart
+    commentContent = ":warning: Coverity Static Analysis found this issue with your code:\n\n" + description + "\n\n[View the full issue report in Coverity](http://synopsys.com)"
 
-  newComment["threadContext"] = threadContext
+    comment_body = f"**Coverity found issue: {checkerProperties['subcategoryShortDescription']} - CWE-{checkerProperties['cweCategory']}, {checkerProperties['impact']} Severity**\n\n"
+    # BAD_CERT_VERIFICATION: The "checkServerIdentity" property in the "tls.connect()" function uses bad cert verification.
+    #comment_body += f"**{checkerProps['subcategoryLocalEffect']}**\n\n"
 
-  newComment["status"] = "active"
+    if (description):
+        comment_body += f"**{issue['checkerName']}**: {description} {checkerProperties['subcategoryLocalEffect']}\n\n"
+    else:
+        comment_body += f"**{issue['checkerName']}**: {checkerProperties['subcategoryLocalEffect']}\n\n"
 
-  azComments.append(newComment)
+    if remediation:
+        comment_body += f"**How to fix:** {remediation}\n"
+
+    comment_body += f"\n<!-- Coverity {issue['mergeKey']} -->\n"
+
+    comment["content"] = comment_body
+    comments.append(comment)
+    newComment["comments"] = comments
+
+    threadContext = dict()
+
+    rightFileEnd = dict()
+    rightFileEnd["line"] = start_line
+    rightFileEnd["offset"] = 1
+
+    rightFileStart = dict()
+    rightFileStart["line"] = start_line
+    rightFileStart["offset"] = 1
+
+    threadContext["filePath"] = "/" + location["file"]
+    threadContext["rightFileEnd"] = rightFileEnd
+    threadContext["rightFileStart"] = rightFileStart
+
+    newComment["threadContext"] = threadContext
+
+    newComment["status"] = "active"
+
+    azComments.append(newComment)
 
 # Ad commensts to PR
 SYSTEM_COLLECTIONURI = os.getenv('SYSTEM_COLLECTIONURI')
@@ -104,20 +194,21 @@ accessToken = os.getenv('SYSTEM_ACCESSTOKEN')
 authorization = str(base64.b64encode(bytes(':' + accessToken, 'ascii')), 'ascii')
 
 headers = {
-  'Accept': 'application/json',
-  'Authorization': 'Basic '+ authorization
+    'Accept': 'application/json',
+    'Authorization': 'Basic ' + authorization
 }
 
 for comment in azComments:
-  if (debug): print("DEBUG: perform API Call to ADO" + url +" : " + json.dumps(comment, indent = 4, sort_keys=True) + "\n")
-  r = requests.post(url=url, json=comment, headers=headers)
-  if r.status_code == 200:
-    if (debug): print("DEBUG: Success")
-  else:
-    print(f"ERROR: Unable to post PR comment to Azure DevOps. Error code: {r.status_code}")
-    print(r.text)
-    sys.exit(1)
+    if (debug): print(
+        "DEBUG: perform API Call to ADO" + url + " : " + json.dumps(comment, indent=4, sort_keys=True) + "\n")
+    r = requests.post(url=url, json=comment, headers=headers)
+    if r.status_code == 200:
+        if (debug): print("DEBUG: Success")
+    else:
+        print(f"ERROR: Unable to post PR comment to Azure DevOps. Error code: {r.status_code}")
+        print(r.text)
+        sys.exit(1)
 
-#if (len(azComments) > 0):
+# if (len(azComments) > 0):
 #  print("INFO: New security weaknesses found, returning exit code 1 to break the build")
 #  sys.exit(1)
